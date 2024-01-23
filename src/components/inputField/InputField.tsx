17fbc8a8545fcb3bc9103b1093de750ca1884b33
Paweł Jarecki <paweljarecki10@gmail.com>
2024-01-23 20:27:15 +0100

    Now background color of text is being validated - spaces can be seen as correct or wrong

--- src/components/inputField/InputField.tsx
+++ src/components/inputField/InputField.tsx
@@ -1,15 +1,12 @@
-import React, {JSX, useEffect, useState} from "react";
+import React, {JSX, useState} from "react";
 
 export const InputField = () => {
 
     const [writtenText, setWrittenText] = useState<string>('')
     const textInput:string = 'HEJKA CO TAM U WAS';
-    const [correctCount, setCorrectCount] = useState<number>(0);
 
     function checkLetter(e:string) {
         setWrittenText(e)
-        const count = e.split('').filter((char, index) => char === textInput[index]).length;
-        setCorrectCount(count);
     }
 
     const highlightedLetters = (textInput: string, writtenText: string): React.JSX.Element[] => {
@@ -21,18 +18,13 @@ export const InputField = () => {
 
             const isCorrect = originalLetter === typedLetter;
 
-            result.push(<span key={i} style={{ color: isCorrect ? 'green' : 'red' }}>{originalLetter}</span>);
+            result.push(<span key={i} style={{ color: isCorrect ? 'green' : 'red', backgroundColor: isCorrect ? 'lightgreen' : 'lightpink'}}>{originalLetter}</span>);
 
         }
         result.push((writtenText.length === 0) ? <span>{textInput}</span> : <span>{textInput.slice(writtenText.length)}</span>)
 
         return result
     }
-
-    useEffect(() => {
-        console.log('Aktualna wartość correctCount:', correctCount);
-        console.log('Proce:', correctCount);
-    }, [correctCount]);
 
     return <>
         {highlightedLetters(textInput, writtenText)}
