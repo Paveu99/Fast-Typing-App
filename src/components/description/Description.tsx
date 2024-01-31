64e912c67d9850b1b606a7a21af332ac2b27d980
Paweł Jarecki <paweljarecki10@gmail.com>
2024-01-31 19:24:05 +0100

    Description written

--- src/components/description/Description.tsx
+++ src/components/description/Description.tsx
@@ -1,5 +1,5 @@
 export const Description = () => {
-    return <p>
-        Hej
+    return <p style={{textAlign: "justify"}}>
+        Hi this is a responsive web application, which helps you get faster at typing! All you have to do is to choose a duration of time and randomly select a text. That's all. Have fun!
     </p>
 }
