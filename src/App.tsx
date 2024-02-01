fa13a80a227e6f8358901af6b89cfdcc546456ca
Paweł Jarecki <paweljarecki10@gmail.com>
2024-02-01 19:05:00 +0100

    Better validation of input data:
    - now values containing are no longer possible to type in

--- src/App.tsx
+++ src/App.tsx
@@ -17,40 +17,60 @@ function App() {
             <header>
                 <AppName/>
             </header>
-            <section style={{display: "flex", justifyContent: "center"}}>
+            <section style={{display: "flex", justifyContent: "center", width: "93vw", margin: 'auto'}}>
                 <Description/>
             </section>
             <section>
                 <div className="time-choosing">
-                    <label style={{fontSize: "25px"}}>How long would like to type?</label>
+                    <div className="question">
+                        <label style={{fontSize: "25px"}}>How long would like to type?</label>
+                        <div className="buttons">
+                            <button
+                                className="time-shortcut"
+                                onClick={() => checkInput(30)}
+                            >
+                                30
+                            </button>
+                            <button
+                                className="time-shortcut"
+                                onClick={() => checkInput(60)}
+                            >
+                                60
+                            </button>
+                            <button
+                                className="time-shortcut"
+                                value={90}
+                                onClick={() => checkInput(90)}
+                            >
+                                90
+                            </button>
+                            <button
+                                className="time-shortcut"
+                                value={90}
+                                onClick={() => checkInput(120)}
+                            >
+                                120
+                            </button>
+                        </div>
+                    </div>
                     <input
                         type="number"
-                        value={timeChosen}
-                        onChange={e => checkInput(Number(e.target.value))}
-                        min={20}
+                        value={(timeChosen < 1) ? '' : timeChosen}
+                        onChange={(e) => {
+                            const rawValue = e.target.value;
+
+                            if (/^\d*$/.test(rawValue) || rawValue === '') {
+                                const newValue = rawValue === '' ? 0 : Math.min(Number(rawValue), 999);
+                                checkInput(newValue);
+                            }
+                        }}
                         className="time-input"
+                        max={999}
                     />
-                    <button
-                        className="time-shortcut"
-                        onClick={() => checkInput(30)}
-                    >
-                        30
-                    </button>
-                    <button
-                        className="time-shortcut"
-                        onClick={() => checkInput(60)}
-                    >
-                        60
-                    </button>
-                    <button
-                        className="time-shortcut"
-                        value={90}
-                        onClick={() => checkInput(90)}
-                    >
-                        90
-                    </button>
                 </div>
-                <InputCheck option={timeChosen}/>
+                <InputCheck
+                    option={timeChosen}
+                />
             </section>
         </div>
     )
